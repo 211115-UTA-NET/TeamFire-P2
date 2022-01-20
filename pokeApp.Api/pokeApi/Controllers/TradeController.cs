2f24f38a927d42413fd86a700cd0c33bac7b3880
Jingjing Zhuang <jzhuang3219@gmail.com>
2022-01-20 12:29:45 -0500

    Sdded required attribute to all PokeApi param

---
+++ pokeApp.Api/pokeApi/Controllers/TradeController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using pokeApi.Data;
+using pokeApi.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace pokeApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TradeController : ControllerBase
+    {
+        private readonly IRepository _repository;
+
+        public TradeController(IRepository repository)
+        {
+            _repository = repository;
+        }
+        //============ GET ===============//
+        [HttpGet]
+        [ActionName("GetByName")]
+        public async Task<IActionResult> GetRecentTradesAsync([FromQuery, Required] string name)
+        {
+            IEnumerable<dtoTradeRecord> Records = await _repository.GetRecentTradesAsync(name);
+            return new JsonResult(Records);
+        }
+
+        [HttpGet]
+        [ActionName("GetByTradeId")]
+        public async Task<IActionResult> GetRecentTradesAsync([FromQuery, Required] int tradeId)
+        {
+            IEnumerable<dtoTradeRecord> Records = await _repository.GetRecentTradesAsync(tradeId);
+            return new JsonResult(Records);
+        }
+
+        [HttpGet]
+        [ActionName("GetAll")]
+        public async Task<IActionResult> GetRecentTradesAsync()
+        {
+            IEnumerable<dtoTradeRecord> Records = await _repository.GetRecentTradesAsync();
+            return new JsonResult(Records);
+        }
+
+
+
+        //==============POST==============///
+        [HttpPost]
+        [ActionName("GetDetail")]
+        public async Task<IActionResult> AddNewRecordAsync([FromQuery, Required] int tradeId, [Required] int cardId, [Required] int offeredId)
+        {
+            IEnumerable<dtoTradeRecord> record = await _repository.AddNewRecordAsync(tradeId, cardId, offeredId);
+            return new JsonResult(record);
+        }
+
+        [HttpPost]
+        [ActionName("GetRecord")]
+        public async Task<IActionResult> AddNewRecordAsync([FromQuery, Required] int offeredByID, [Required] int recevedByID)
+        {
+            IEnumerable<dtoTradeRecord> record = await _repository.AddNewRecordAsync(offeredByID, recevedByID);
+            return new JsonResult(record);
+        }
+    }
+}
---
+++ pokeApp.Api/pokeApi/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using pokeApi.Data;
+using pokeApi.Models;
+
+namespace pokeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IRepository _repository;
+
+        public UserController(IRepository repository)
+        {
+            _repository = repository;
+        }
+        //============ GET ===============//
+        [HttpGet]
+        public async Task<IActionResult> GetAllByNameAsync([FromQuery, Required] string name)
+        {
+            IEnumerable<dtoUser> Users = await _repository.GetUsersAsync(name);
+            return new JsonResult(Users);
+        }
+
+        //==============POST==============//
+        [HttpPost]
+        public async Task<IActionResult> AddNewUserAsync([FromQuery, Required] string name, [Required]string pw, [Required] string email)
+        {
+            IEnumerable<dtoUser> Users = await _repository.AddNewUserAsync(name, pw, email);
+            return new JsonResult(Users);
+        }
+
+
+    }
+}
