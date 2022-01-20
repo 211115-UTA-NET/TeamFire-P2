c141a83c286b3931a75f37d14466f7f2da416ff4
Jingjing Zhuang <jzhuang3219@gmail.com>
2022-01-20 12:07:08 -0500

    create a new branch pokeApi for api dev

--- pokeApi/Models/dtoTradeRecord.cs
+++ pokeApi/Models/dtoTradeRecord.cs
@@ -1,18 +1,49 @@
 ﻿using System;
 namespace pokeApi.Models
 {
-	public class dtoTradeRecord
-	{
+    public class dtoTradeRecord
+    {
 
-		public int tradeID { get; set; }
-		public string offeredBy { get; set; }
-		public string redeemedBy { get; set; }
+        public int tradeID { get; set; }
+        public string offeredBy { get; set; }
+        public int offeredByID { get; set; }
+        public string redeemedBy { get; set; }
+        public int redeemedByID { get; set; }
+        public int pokeID { get; set; }
+        public string pokemon { get; set; }
+        public int cardId { get; set; }
+
+
 
         public dtoTradeRecord(int tradeID, string offeredBy, string redeemedBy)
         {
             this.tradeID = tradeID;
             this.offeredBy = offeredBy;
             this.redeemedBy = redeemedBy;
+        }
+
+        public dtoTradeRecord(int tradeID, int offeredByID, int redeemedByID)
+        {
+            this.tradeID = tradeID;
+            this.offeredByID = offeredByID;
+            this.redeemedByID = redeemedByID;
+        }
+
+        public dtoTradeRecord(string offeredBy, string pokemon)
+        {
+            this.offeredBy = offeredBy;
+            this.pokemon = pokemon;
+        }
+
+        public dtoTradeRecord(int offeredByID, int redeemedByID)
+        {
+            this.offeredByID = offeredByID;
+            this.redeemedByID = redeemedByID;
+        }
+
+        public dtoTradeRecord(int cardId)
+        {
+            this.cardId = cardId;
         }
     }
 }
---
+++ pokeApiTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace pokeApiTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
