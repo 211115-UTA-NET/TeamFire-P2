7cc5c53cdb726825e386e6e09d0e96e8886d0ada
Ben Herbert <50344850+ben22020@users.noreply.github.com>
2022-01-20 22:38:43 -0500

    typescript models

---
+++ pokeApp.UI/poke-app/src/Pokemon.ts
@@ -0,0 +1,6 @@
+export interface Pokemon {
+  	
+  pokeID: number
+  pokemon: string;
+
+}
---
+++ pokeApp.UI/poke-app/src/User.ts
@@ -0,0 +1,8 @@
+export interface User {
+
+  userID: number;
+  userName: string;
+  password: string;
+  email: string;
+
+}
