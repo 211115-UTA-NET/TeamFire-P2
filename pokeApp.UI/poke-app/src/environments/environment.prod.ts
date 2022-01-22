9b9e665fd3ba7dea3ee873c25ec7baeee34e355c
Jingjing Zhuang <jzhuang3219@gmail.com>
2022-01-22 00:20:17 -0500

    Edited environment.prod.ts

--- pokeApp.UI/poke-app/src/environments/environment.prod.ts
+++ pokeApp.UI/poke-app/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+import authInfo from '../../auth_config.json';
+
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain: authInfo.domain,
+    clientId: authInfo.clientId,
+    redirectUri: window.location.origin,
+  },
 };
